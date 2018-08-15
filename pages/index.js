4ab6b88d4c0358ed60f825146c86edd5c438a575
decent_descent <radl.honza@gmail.com>
2018-08-15 12:19:13 +0200

    GTM

--- pages/index.js
+++ pages/index.js
@@ -79,6 +79,19 @@ export default class Index extends React.Component {
 
     ReactGA.initialize("UA-50377630-2");
     ReactGA.pageview(window.location.pathname + window.location.search);
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        "gtm.start": new Date().getTime(),
+        event: "gtm.js"
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-5DMDW8M");
   }
 
   render() {
