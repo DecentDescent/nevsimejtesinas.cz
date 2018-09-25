a807691172013ab2c851be9f18f045082753a062
decent_descent <radl.honza@gmail.com>
2018-09-25 13:38:39 +0200

    New Info Modal

--- components/InfoModal.js
+++ components/InfoModal.js
@@ -4,12 +4,12 @@ export default class InfoModal extends React.Component {
       <div className="info-modal">
         <div className="info-modal-content">
           <p>
-            Ivan usnul, protože ho ovládla spavá nemoc. Chcete vědět víc? Tady
-            máte příležitost.
+            Usnul, protože ho ovládla spavá nemoc. Chcete vědět víc? Tady máte
+            příležitost.
           </p>
           <p>
             Musíme vás však varovat. Chystáte se vstoupit do říše démonů.
-            Opomíjených netvorů se kterými už léta bojujeme.
+            Opomíjených netvorů, se kterými už léta bojujeme.
           </p>
           <a className="btn btn--share" href="https://www.nevsimejtesinas.cz">
             I přesto chci vstoupit
